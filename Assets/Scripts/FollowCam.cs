229d7ce22a8713bf5541d0f1523d1f6c329910e1
Jorge Alejos <jalejos@nearsoft.com>
2016-04-19 13:57:09 -0600

    presesion

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	public static FollowCam cam;
+	public GameObject poi;
+	Vector3 camPos;
+	float easing;
+
+	void Awake(){
+		cam = this;
+		camPos = this.transform.position;
+		easing = 0.05f;
+	}
+
+	void FixedUpdate(){
+		if (poi == null)
+			return;
+		Vector3 pos = poi.transform.position;
+		pos.z = camPos.z;
+		pos.x = Mathf.Max (camPos.x, pos.x);
+		pos.y = Mathf.Max (camPos.y, pos.y);
+
+		pos = Vector3.Lerp (this.transform.position, pos, easing);
+		this.transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/SlingshotScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingshotScript : MonoBehaviour {
+
+	GameObject projectile;
+	public GameObject prefabProjectile;
+	GameObject launchPoint;
+	Vector3 launchPos;
+	bool aimingMode;
+	public float velocity;
+
+	void Awake () {
+		Transform transformLaunchPoint = transform.Find ("LaunchPoint");
+		launchPoint = transformLaunchPoint.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = transformLaunchPoint.position;
+	}
+
+	void FixedUpdate () {
+		if (!aimingMode) {
+			return;
+		}
+		Vector3 mouseScreenPos = Input.mousePosition;
+		mouseScreenPos.z = -Camera.main.transform.position.z;
+		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint (mouseScreenPos);
+		Vector3 mouseDelta = mouseWorldPos - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+		Vector3 projectilePos = mouseDelta + launchPos;
+		projectile.transform.position = projectilePos;
+
+		if (Input.GetMouseButtonUp (0)) {
+			aimingMode = false;
+			projectile.GetComponent<Rigidbody> ().velocity = -mouseDelta * velocity;
+			projectile.GetComponent<Rigidbody> ().isKinematic = false;
+			FollowCam.cam.poi = projectile;
+			projectile = null;
+		}
+	}
+
+	void OnMouseEnter () {
+		if(!aimingMode)
+			launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit () {
+		if(!aimingMode)
+			launchPoint.SetActive (false);
+	}
+
+	void OnMouseDown () {
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.GetComponent<Rigidbody> ().isKinematic = true;
+		aimingMode = true;
+	}
+}
